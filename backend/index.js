f9001cbe38c26c33f26093d3f698c411b9d73968
akanksha4022 <akanksha70010@gmail.com>
2025-11-20 00:47:30 +0530

    added route file

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,9 @@ app.use(cors());
 
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.get("/", (req, res) => {
+  res.send("Backend running successfully!");
+});
 
 app.get('/ping',(req, res)=>{
     res.send('PONG');
